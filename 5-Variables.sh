340f9ceb764ab48e3abaf218235141e3f3b58b4b
sai <sai@gmail.com>
2024-12-19 17:26:55 +0530

    first commit

---
+++ 5-Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Please enter username::"
+read USERNAME
+echo "Username enterd is $USERNAME"
+
+echo "Please enter password::"
+read -s PASSWORD
+echo "Password entered is "$PASSWORD"
